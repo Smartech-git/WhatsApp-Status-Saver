ea84526c0128857978aee1f5fa0fa802d8ff7ede
Smartech-git <chukwusmartchukwuemeka@gmail.com>
2022-10-12 01:16:12 +0100

    update on bottom nav bar and UI

--- Components/BottomNavTabBar.js
+++ Components/BottomNavTabBar.js
@@ -1,32 +1,72 @@
+import { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, Image} from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withDelay, withSpring, withTiming } from 'react-native-reanimated';
 import { useStateValue } from '../StateProvider';
+
+export let setShouldTabHideRef;  // imported in Utilities/GestureHandler.js
 
 export default function BottomNavTabBar({ state, descriptors, navigation }) {
   const [State, dipatch] = useStateValue()
+  const [shouldTabHide, setShouldTabHide] = useState('')
+  const translation = useSharedValue(0)
+  const opacity = useSharedValue(0)
+
+
+ const shouldTabHide_Animation = (trans, opac) => {
+    translation.value = withSpring(trans, {mass: 0.8})
+      opacity.value = withTiming(opac , {
+        duration: 300,
+      })
+  }
+
+  useEffect(() => {
+    setShouldTabHideRef = setShouldTabHide
+    translation.value = withDelay(900, withSpring(-35, {mass: 0.8}))
+    opacity.value = withDelay(900, withTiming(1, {
+      duration: 300,
+    }))
+  }, [])
+
+  useEffect(() => {
+    if(shouldTabHide === 'true') {
+     shouldTabHide_Animation(0, 0)
+    } else if( shouldTabHide === 'false') {
+     shouldTabHide_Animation(-30, 1)
+    }   
+  }, [shouldTabHide])
+
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: translation.value
+        }
+      ],
+      opacity: opacity.value
+    }
+  })
 
   return (
-    <View style={{
-      backgroundColor: State.themeHue.primary,
-      alignItems: 'center'
-    }}>
-      <View style={{
-        alignItems: 'center',
-        width: '102%',
-        backgroundColor: State.themeHue.primary_dark,
-        borderColor: State.themeHue.secondary_sub,
-        borderTopWidth: 2,
-        borderLeftWidth: 2,
-        borderRightWidth: 2,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        paddingTop: 8
-      }}>
+      <Animated.View style={[
+        {
+        position: 'absolute',
+        bottom: 0,
+        width:  '100%',
+        alignItems: 'center'
+      },
+      animatedStyle
+    ]}>
         <View style={{
-            width: 300,
-            height: 50,
+            height: 70,
+            width: 260,
             flexDirection: 'row',
             justifyContent: 'space-around',
+            alignItems: 'center',
+            backgroundColor: State.themeHue.primary_dark,
             borderColor: State.themeHue.secondary_sub,
+            borderWidth: 1,
+            borderRadius: 50
         }}>
           {state.routes.map((route, index) => {
             const { options } = descriptors[route.key];
@@ -100,7 +140,6 @@ export default function BottomNavTabBar({ state, descriptors, navigation }) {
             );
           })}
         </View>
-      </View>
-    </View>
+      </Animated.View>
   );
 }
