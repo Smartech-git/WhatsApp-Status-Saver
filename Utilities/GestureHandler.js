d1103b00c9c2a0606ab8d0d031ff05785b3fc167
Smartech-git <chukwusmartchukwuemeka@gmail.com>
2022-10-12 01:30:11 +0100

    added bottomTabNav onScroll hide functionality

--- Utilities/GestureHandler.js
+++ Utilities/GestureHandler.js
@@ -1,10 +1,25 @@
+import { setShouldTabHideRef } from "../Components/BottomNavTabBar";
+let contentOffsetState = 0;
+
 export const handleOnScroll = (e, setOffsetTop, setOffsetBottom) => {
-  setOffsetTop(e.nativeEvent.contentOffset.y)
-  setOffsetBottom(e.nativeEvent.contentSize.height -  e.nativeEvent.layoutMeasurement.height)
+  let offsetY = e.nativeEvent.contentOffset.y
+  console.log(offsetY)
+  setOffsetTop(offsetY)
+  setOffsetBottom(e.nativeEvent.contentSize.height - e.nativeEvent.layoutMeasurement.height)
+
+  if(offsetY > (contentOffsetState)){
+    setShouldTabHideRef('true')
+  } else if(offsetY < (contentOffsetState)){
+    setShouldTabHideRef('false')
+  }
 }
 
 export const panGestureConditional = (offsetTop, offsetBottom) => {
   return offsetTop === 0 ? [-30, 0]
         : offsetTop === offsetBottom ? [0, 30]
-        : [-200, 200]
+        : [-500, 500]
+}
+
+export const handleScrollEndDrag = (e) => {
+  contentOffsetState = e.nativeEvent.contentOffset.y
 }
