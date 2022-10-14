c1fb8df502849099802de2cb7fe1ebfe4ca2493c
Smartech-git <chukwusmartchukwuemeka@gmail.com>
2022-10-14 22:04:04 +0100

    update on functions

--- Utilities/GetViewedStatus.js
+++ Utilities/GetViewedStatus.js
@@ -1,7 +1,16 @@
+import * as FileSystem from 'expo-file-system';
+
+const FILE_PATH = {
+    WhatsApp : "file:///storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Statuses/",
+    WhatsApp4B : ""
+}
+
+const regEx = /(jpg|png)$/g;
+
 export const ImageArray = [
-    {
-        image : require('../Test/Images/image1.png')
-    },
+    // {
+    //     image : require(FILE_PATH.WhatsApp + '12f431eb564a43d1b2bd5716440652c3.jpg')
+    // },
     {
         image : require('../Test/Images/image2.jpg')
     },
@@ -24,21 +33,33 @@ export const ImageArray = [
         image : require('../Test/Images/image8.jpg')
     },
     {
-        image : require('../Test/Images/image8.jpg')
+        image : require('../Test/Images/image1.png')
     },
     {
-        image : require('../Test/Images/image8.jpg')
+        image : require('../Test/Images/image2.jpg')
     },
     {
-        image : require('../Test/Images/image8.jpg')
+        image : require('../Test/Images/image3.jpg')
     },
-    {
-        image : require('../Test/Images/image8.jpg')
-    },
-    {
-        image : require('../Test/Images/image8.jpg')
-    },
-    {
-        image : require('../Test/Images/image8.jpg')
-    },
-]
+   
+];
+
+export let viewedStatusImagesArr = []
+
+export const getViewedStatusImages = async () => {
+
+    const Dir  = FileSystem.documentDirectory;
+    const album = await FileSystem.readDirectoryAsync(FILE_PATH.WhatsApp)
+    const fileInfo = await FileSystem.getInfoAsync(FILE_PATH.WhatsApp)
+
+    album.forEach(item => {
+        if(item.endsWith('jpg')){
+            viewedStatusImagesArr.push(
+                {
+                    URL: FILE_PATH.WhatsApp + item
+                }
+            )
+        } else {
+        }
+    });
+}
