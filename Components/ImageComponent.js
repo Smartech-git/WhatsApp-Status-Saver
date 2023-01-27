6c257e127f90ce3c6f523bf5386d113f91a9ef01
Smartech-git <chukwusmartchukwuemeka@gmail.com>
2023-01-27 17:18:45 +0100

    Initailized tap to view Image component

--- Components/ImageComponent.js
+++ Components/ImageComponent.js
@@ -4,16 +4,20 @@ import React from 'react'
 const win = Dimensions.get('window').width-20
 
 export default function ImageComponent({imageSrc}) {
+
   return (
     <View style={{
         width: win,
-        height: '100%',
         marginHorizontal: 10,
         borderRadius: 10,
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center' ,
         overflow: 'hidden'
     }}>
-      <ImageBackground source={{uri: imageSrc}} blurRadius={500} resizeMode ='cover' style={{ height: '100%'}}>
-        <Image style={{width: '100%', flex: 1}} source={{uri: imageSrc}} resizeMode={'contain'} />
+      <ImageBackground source={{uri: imageSrc}} blurRadius={500} resizeMode= 'cover' style={{ height: '100%', width: '100%',
+    }}>
+        <Image style={{flex: 1}} source={{uri: imageSrc}} resizeMode={'contain'} />
       </ImageBackground>
     </View>
   )
