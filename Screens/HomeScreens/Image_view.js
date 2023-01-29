2cdcd5d172838a69530c39bed7b3cde4e89b8e84
Smartech-git <chukwusmartchukwuemeka@gmail.com>
2023-01-29 18:59:17 +0100

    Image pager implimentation problem resolved

--- Screens/HomeScreens/Image_view.js
+++ Screens/HomeScreens/Image_view.js
@@ -1,20 +1,22 @@
-import { View, Text, Dimensions } from 'react-native'
-import React, {useState} from 'react'
+import { View,  Dimensions} from 'react-native'
+import React, {useState } from 'react'
 import { useStateValue } from '../../StateProvider'
 import { viewedImagesArr } from '../../Utilities/ViewedStatusManager';
-import { FlashList } from '@shopify/flash-list';
 import ImageComponent from '../../Components/ImageComponent';
 import { handleHorinzontalScroll, panGestureConditional } from '../../Utilities/GestureHandler';
 import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler'
+import PagerView from 'react-native-pager-view';
 
-const win = Dimensions.get('window').width-20
+const width = Dimensions.get('window').width-20
+const offset = Dimensions.get('window').width;
 
-export default function Image_view() {
+export default function Image_view({route}) {
     const [contentOffsetLeft, setContentOffsetLeft] = useState(0)
     const [contentOffsetRight, setContentOffsetRight] = useState(0)
     const [state, dispatch] = useStateValue();
     const startPosition = useSharedValue(0);
+    
 
     const animatedStyle = useAnimatedStyle(() => {
         return {
@@ -23,6 +25,7 @@ export default function Image_view() {
           ]
         }
       })
+    
     
     const panGestureEvent = Gesture.Pan()
     .maxPointers(1)
@@ -35,31 +38,28 @@ export default function Image_view() {
     startPosition.value =  e.translationX* 0.5
     })
     .onEnd(() =>{
-    startPosition.value = withSpring(0, {mass: 0.8})
+    startPosition.value = withSpring(0, {mass: 1})
     })
 
     return (
-        <View style={{ 
+        <View style={{
             flex: 1
         }}>
             <View style={{marginVertical: 20, flex: 1}} >
-                <GestureDetector gesture={panGestureEvent}>
-                    <Animated.View style={animatedStyle}>
-                        <FlashList
-                            data={viewedImagesArr}
-                            renderItem={({item})=> <ImageComponent imageSrc={item.URL}/>}
-                            extraData={viewedImagesArr.length}
-                            horizontal = {true}
-                            estimatedItemSize={100}
-                            decelerationRate = 'normal'
-                            persistentScrollbar = {false}
-                            overScrollMode = 'never'
-                            showsHorizontalScrollIndicator = {false}
-                            pagingEnabled = {true}
-                            onScroll={(e) => handleHorinzontalScroll(e, contentOffsetRight, setContentOffsetLeft, setContentOffsetRight)}
-                        />
-                    </Animated.View>
-                </GestureDetector>
+                <PagerView style={{flex: 1}} 
+                    initialPage={route.params.index}
+                    offscreenPageLimit ={3}
+                    overScrollMode='never'
+                >
+                    {
+                        viewedImagesArr.map((item, index) => {
+                            return (
+                                <ImageComponent imageSrc={item.URL} key={index} /> 
+                            )
+                        
+                        })
+                    }
+                </PagerView>
             </View>
             <View style={{height: 100, width: '100%'}}>
 
