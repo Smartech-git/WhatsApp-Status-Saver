25c642b9033e422f7cbaed8b48847e73066e9942
Smartech-git <chukwusmartchukwuemeka@gmail.com>
2023-04-29 00:56:01 +0100

    Components specializations and modifications

---
+++ Components/ContentViewOptionsVideo.js
@@ -0,0 +1,78 @@
+import { View, Text, StyleSheet, TouchableOpacity, PixelRatio, Image, Pressable } from 'react-native'
+import React, {useState} from 'react';
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withSpring, withTiming} from 'react-native-reanimated'
+import { useStateValue } from '../StateProvider';
+import { ZoomIn } from 'react-native-reanimated';
+
+export default function ContentViewOptionsVideo() {
+  const [state, dispatch] = useStateValue()
+  const [pressed, setPressed] = useState(false)
+  const scaleValue = useSharedValue(0.5)
+
+  const saveButtonAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{
+        scale: scaleValue.value
+      }]
+    }
+  })
+
+  const handleSave = () => {
+    setPressed(true)
+    scaleValue.value = withTiming(1, {
+      duration: 800,
+      easing: Easing.elastic(3)
+    })
+  } 
+
+  return (
+    <View style={Styles.Content}>
+            <View style={{
+                alignItems: 'center'
+            }}>
+                <Pressable onPress={handleSave} >
+                  <View style={[Styles.Botton, {backgroundColor: pressed ? '#00D426' : '#FFFFFF00'}]}>
+                    {
+                    pressed ? <Animated.Image style={[{width: PixelRatio.getPixelSizeForLayoutSize(12), height: PixelRatio.getPixelSizeForLayoutSize(12)}, saveButtonAnimatedStyle]} source={require('../assets/Icons/SavedIcon.png')} />
+                            : <Image style={{width: PixelRatio.getPixelSizeForLayoutSize(10), height: PixelRatio.getPixelSizeForLayoutSize(10)}} source={require('../assets/Icons/SaveIcon.png')}/>
+                    }    
+                  </View>
+                </Pressable>
+                <Text style={{fontSize: 14, fontWeight: '700', color: state.theme === 'LIGHT' ? '#000' : '#FFF'}}>{pressed ? "Saved": "Save"}</Text> 
+            </View>
+
+            <View style={{
+                alignItems: 'center'
+            }}>
+
+                <TouchableOpacity activeOpacity={0.6} style={{ ...Styles.Botton,
+                    backgroundColor: undefined,
+                }}>
+                    <Image style={{width: PixelRatio.getPixelSizeForLayoutSize(12), height: PixelRatio.getPixelSizeForLayoutSize(12)}} source={require('../assets/Icons/ShareIcon.png')} />
+                   
+                </TouchableOpacity>
+                <Text style={{fontSize: 14, fontWeight: '700', color: state.theme === 'LIGHT' ? '#000' : '#FFF'}}>Share</Text> 
+            </View>
+    </View>
+  )
+}
+
+const Styles = StyleSheet.create({
+    Content: {
+        height: 160,
+        justifyContent: 'space-between',
+        position: 'absolute',
+        zIndex: 3,
+        bottom: 100,
+        right: 15,
+        alignItems: 'center',
+    },
+
+    Botton: {
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
